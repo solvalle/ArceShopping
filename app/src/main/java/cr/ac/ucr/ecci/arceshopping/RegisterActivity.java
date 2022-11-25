aa6627ab8fe66e315e4bc3395af966c8ce47d81d
asdru333 <asdrubal.villegas.molina@hotmail.com>
2022-11-24 23:59:55 -0600

    GPS function changed to work as intended

--- app/src/main/java/cr/ac/ucr/ecci/arceshopping/RegisterActivity.java
+++ app/src/main/java/cr/ac/ucr/ecci/arceshopping/RegisterActivity.java
@@ -2,6 +2,7 @@ package cr.ac.ucr.ecci.arceshopping;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -10,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.util.Patterns;
 import android.widget.ArrayAdapter;
@@ -37,14 +39,22 @@ import cr.ac.ucr.ecci.arceshopping.db.DbUsers;
 import cr.ac.ucr.ecci.arceshopping.model.EmailManager;
 import cr.ac.ucr.ecci.arceshopping.model.User;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
 public class RegisterActivity extends ConnectedActivity {
     private TextInputLayout id;
     private TextInputLayout completeName;
     private TextInputLayout email;
     private TextInputLayout age;
     private Spinner province;
+    ProvinceCalculator provinceCalculator;
     private double latitude;
     private double longitude;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,19 +65,13 @@ public class RegisterActivity extends ConnectedActivity {
         this.email = (TextInputLayout) findViewById(R.id.register_email);
         this.age = (TextInputLayout) findViewById(R.id.register_age);
         this.province = (Spinner) findViewById(R.id.register_province_spinner);
-        ProvinceCalculator provinceCalculator = new ProvinceCalculator();
-
+        provinceCalculator = new ProvinceCalculator();
         ArrayList<String> provinces = new ArrayList<String>(Arrays.asList("San José", "Alajuela", "Cartago", "Heredia",
                 "Guanacaste", "Puntarenas", "Limón"));
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 com.google.android.material.R.layout.support_simple_spinner_dropdown_item, provinces);
         this.province.setAdapter(adapter);
-
-        if (!getLocation()) {
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
-        }
-        this.province.setSelection(provinceCalculator.calculateProvince(latitude, longitude));
+        checkPermissions();
     }
 
     public void register(View view) {
@@ -165,38 +169,62 @@ public class RegisterActivity extends ConnectedActivity {
         return true;
     }
 
-    public boolean getLocation()
-    {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED) {
-            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null) {
-                this.latitude = location.getLatitude();
-                this.longitude = location.getLongitude();
-            }
-            else {
-                this.latitude = 0;
-                this.longitude = 0;
-            }
-            return true;
+    public void checkPermissions() {
+
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        } else {
+            getLocation();
         }
-        return false;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            getLocation();
-            Toast.makeText(this, "Location Permission Granted", Toast.LENGTH_SHORT).show();
-        } else {
-            this.latitude = 0;
-            this.longitude = 0;
+        if (requestCode == REQUEST_CODE && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
+            } else {
+                Toast.makeText(this, "Permiso Denegado", Toast.LENGTH_SHORT).show();
+                this.latitude = 0;
+                this.longitude = 0;
+                this.province.setSelection(provinceCalculator.calculateProvince(latitude, longitude));
+            }
+        }
+    }
+
+    private void getLocation() {
+        try {
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setInterval(10000);
+            locationRequest.setFastestInterval(3000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(locationRequest,
+                    new LocationCallback()
+            {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    LocationServices.getFusedLocationProviderClient(RegisterActivity.this).removeLocationUpdates(this);
+                    if (locationResult != null && locationResult.getLocations().size() > 0) {
+                        int latestLocationIndex = locationResult.getLocations().size() - 1;
+                        latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                        longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                        System.out.println(latitude + " " + longitude);
+                        province.setSelection(provinceCalculator.calculateProvince(latitude, longitude));
+                    }
+                }
+
+            }, Looper.myLooper());
+
+        }catch (Exception e){
+            System.out.println(e);
         }
     }
 }
